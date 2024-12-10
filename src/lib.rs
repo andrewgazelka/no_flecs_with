e644ca69f5f17ce64cf2b1a0d8a8b924a89cbc0b
Andrew Gazelka <andrew.gazelka@gmail.com>
2024-12-09 23:47:28 -0800

    update (poor)

--- src/lib.rs
+++ src/lib.rs
@@ -2,24 +2,24 @@
 #![feature(let_chains)]
 #![warn(unused_extern_crates)]
 
-extern crate rustc_arena;
+// extern crate rustc_arena;
 
-extern crate rustc_ast;
-extern crate rustc_ast_pretty;
-extern crate rustc_attr;
-extern crate rustc_data_structures;
-extern crate rustc_errors;
+// extern crate rustc_ast;
+// extern crate rustc_ast_pretty;
+// extern crate rustc_attr;
+// extern crate rustc_data_structures;
+// extern crate rustc_errors;
 extern crate rustc_hir;
-extern crate rustc_hir_pretty;
-extern crate rustc_index;
-extern crate rustc_infer;
-extern crate rustc_lexer;
-extern crate rustc_middle;
-extern crate rustc_mir_dataflow;
-extern crate rustc_parse;
-extern crate rustc_span;
-extern crate rustc_target;
-extern crate rustc_trait_selection;
+// extern crate rustc_hir_pretty;
+// extern crate rustc_index;
+// extern crate rustc_infer;
+// extern crate rustc_lexer;
+// extern crate rustc_middle;
+// extern crate rustc_mir_dataflow;
+// extern crate rustc_parse;
+// extern crate rustc_span;
+// extern crate rustc_target;
+// extern crate rustc_trait_selection;
 
 use rustc_hir::{Expr, ExprKind, Path, QPath};
 use rustc_lint::LateLintPass;
@@ -49,15 +49,23 @@ use rustc_lint::LintContext;
 
 impl<'tcx> LateLintPass<'tcx> for NoFlecsWith {
     fn check_expr(&mut self, cx: &rustc_lint::LateContext<'tcx>, expr: &'tcx Expr<'_>) {
-        if let ExprKind::Path(ref qpath) = expr.kind {
-            if let QPath::Resolved(_, path) = qpath {
-                if path_matches_flecs_with(path) {
-                    cx.span_lint(NO_FLECS_WITH, expr.span, |diag| {
-                        diag.note("usage of flecs::With is discouraged");
-                    });
-                }
-            }
-        }
+        let ExprKind::Path(ref qpath) = expr.kind else {
+            return;
+        };
+
+            cx.span_lint(NO_FLECS_WITH, expr.span, |diag| {
+                diag.note("usage of flecs::With is discouraged");
+            });
+
+        // let QPath::Resolved(_, path) = qpath else {
+        //     return;
+        // };
+
+        // if path_matches_flecs_with(path) {
+        //     cx.span_lint(NO_FLECS_WITH, expr.span, |diag| {
+        //         diag.note("usage of flecs::With is discouraged");
+        //     });
+        // }
     }
 }
 
